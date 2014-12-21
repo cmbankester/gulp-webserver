9fd1c43b4a3d0e5443251b2fae9ee41edc68e664
Christian Bankester <cbankester@immense.net>
2014-12-20 21:55:28 -0600

    Fixed an issue with restarting

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-var child_process = require('child_process'),
-    default_opts = {
-      file: 'index.js',
-      args: []
-    };
+var child_process = require('child_process');
 
 
 module.exports = (function () {
-  var service     = undefined,
-      server_file = undefined,
-      node_args   = undefined;
+  var service;
+  var restarting;
+
+  // Intercept ctrl+c and pass it to the service instead of letting it kill
+  // process. That way, any cleanup that the service needs to perform can
+  // log to stdout if necessary (process won't die until after service tells
+  // it that it's dying)
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+  process.stdin.on('data', function(char) {
+    if (char == '\3') {
+      service.kill('SIGINT');
+      service = undefined;
+    } else {
+      process.stdout.write(char);
+    }
+  });
+
+  process.on('exit', function (){
+    if (service) {
+      service.kill();
+    }
+  });
 
   return {
     run: function (opts) {
-      node_args = opts.args || default_opts.args;
-      node_args.push(opts.file || default_opts.file);
+      var node_args = opts.args || [];
 
-      if (service) { // Stop
+      node_args.push(opts.file || 'index.js');
+
+      if (service) {
+        restarting = true;
         service.kill('SIGINT');
         service = undefined;
       }
@@ -26,29 +45,11 @@ module.exports = (function () {
       service.stderr.setEncoding('utf8');
       service.stderr.pipe(process.stderr);
 
-      // Intercept ctrl+c and pass it to the service instead of letting it kill
-      // process. That way, any cleanup that the service needs to perform can
-      // log to stdout if necessary (process won't die until after service tells
-      // it that it's dying)
-      process.stdin.resume();
-      process.stdin.setEncoding('utf8');
-      process.stdin.setRawMode(true);
-      process.stdin.on('data', function(char) {
-        if (char == '\3') {
-          service.kill('SIGINT');
-          service = undefined;
-        } else {
-          process.stdout.write(char);
-        }
-      });
-
       service.on('exit', function(){
-        process.exit(0);
-      });
-
-      process.on('exit', function (){
-        if (service) {
-          service.kill();
+        if (restarting) {
+          restarting = undefined;
+        } else {
+          process.exit(0);
         }
       });
 
